fbf20ef6f876e4afbd912358994ba5bc03782dd0
chuan <zhangchuan.cn@gmail.com>
2015-11-02 18:09:20 +0800

    大小写修改，适应Linux

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,7 +4,7 @@ return array(
 	//数据库配置信息
 	'DB_TYPE'   => 'sqlite', // 数据库类型
 	//'DB_HOST'   => 'localhost', // 服务器地址
-	'DB_NAME'   => './public/data/tastegame_hr', // 数据库名
+	'DB_NAME'   => './Public/data/tastegame_hr', // 数据库名
 	//'DB_USER'   => 'root', // 用户名
 	//'DB_PWD'    => '', // 密码
 	//'DB_PORT'   => 3306, // 端口
